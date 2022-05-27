d039225bc03eb5bf13386e052d9e079d6cd72aba
Ameena <meenamughal886@gmail.com>
2022-05-27 11:54:13 -0700

    done

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp3;
+
+//Computer.ComputerBuilder builder = new Computer.ComputerBuilder("1 TB", "16 GB");
+
+//Computer computer = builder.setGraphicCard("820 M")
+//        .setSpeaker("Bose")
+//        .build(); ;
+User u=new User.UserBuilder()
+    .WithName("John")
+    .WithAddress("123 Street")
+    .Build();
---
+++ ConsoleApp3/User.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public interface IUser
+    {
+        string Name { get; }
+        string Address { get; }
+        int Age { get; }
+    }
+
+    public class User 
+    {
+        private string name;
+        private int age;
+        private string address;
+
+        private User() { }
+
+        public string Name => name;
+
+        public string Address => address;
+
+        public int Age => age;
+
+        public class UserBuilder
+        {
+            private readonly User user;
+
+            public UserBuilder()
+            {
+                user = new User();
+            }
+
+            public UserBuilder WithName(string name)
+            {
+                user.name = name;
+                return this;
+            }
+
+            public UserBuilder WithAge(int age)
+            {
+                user.age = age;
+                return this;
+            }
+
+            public UserBuilder WithAddress(string address)
+            {
+                user.address = address;
+                return this;
+            }
+
+            public User Build() => user;
+        }
+    }
+}
